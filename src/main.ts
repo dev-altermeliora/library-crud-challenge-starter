781f985d727da8cd711b084b61e00b17ee6a35c5
Vladimir Loskutov <vladimir@altermeliora.com>
2023-09-13 17:25:01 +0300

    feat: created nestjs app, added sequelize and postgres

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // global prefix
+  app.setGlobalPrefix('api/v1');
+  await app.listen(3000);
+}
+bootstrap();
